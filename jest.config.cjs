17dc45a648b142746fead7ee4869bb9361843499
Nikita Okrepilov <nokrepilov@yandex.ru>
2024-03-23 15:38:32 +0300

    second mentor check

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,4 +2,7 @@ module.exports = {
   transform: {
     '^.+\\.js$': 'babel-jest',
   },
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.js'], // указывает, какие файлы должны быть включены в отчёт о покрытии
+  coverageReporters: ['json', 'lcov', 'text', 'clover'], // определяет форматы отчётов о покрытии
 };
